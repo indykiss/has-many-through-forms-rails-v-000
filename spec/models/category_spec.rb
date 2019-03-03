9aa98b9b7e5bdf0674ff52f48d9045f2e2986d3b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-03-03 23:27:11 +0000

    Automatically backed up by Learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 # Using local IDE today and need this lab to pass; migration issue?
-#RSpec.describe Category, type: :model do
+# RSpec.describe Category, type: :model do
 #  pending "add some examples to (or delete) #{__FILE__}"
 #end
